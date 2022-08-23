be8b48ed22e3645e59cbd1b525694bb3161fe457
FelipeGomezGarcia <feli.goga@gmail.com>
2022-08-23 09:51:35 +0200

    Initial Commit

---
+++ src/main/java/com/example/api_rest/Ex01ApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex01ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex01ApiApplication.class, args);
+	}
+
+}
